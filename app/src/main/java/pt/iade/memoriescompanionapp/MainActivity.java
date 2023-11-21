26977d9b5caefd43dc569454bacb014d32d42e7a
John <accounts.8mr28@aleeas.com>
2023-11-21 18:02:38 +0000

    Shake Detection
    - Shake detection using accelerometer working
    - Few changes in the main activity

--- app/src/main/java/pt/iade/memoriescompanionapp/MainActivity.java
+++ app/src/main/java/pt/iade/memoriescompanionapp/MainActivity.java
@@ -2,13 +2,88 @@ package pt.iade.memoriescompanionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button petSelectButton;
+    private Button myProfileButton;
+
+    private int place;
+
+    private SensorManager sensorManager;
+    private Sensor accelSensor;
+
+    private static final int SHAKE_THRESHOLD = 800;
+    private long lastUpdate;
+    private float x;
+    private float y;
+    private float z;
+    private float last_x;
+    private float last_y;
+    private float last_z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupComponents();
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (accelSensor == sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)) {
+                    long curTime = System.currentTimeMillis();
+                    // only allow one update every 100ms.
+                    if ((curTime - lastUpdate) > 100) {
+                        long diffTime = (curTime - lastUpdate);
+                        lastUpdate = curTime;
+
+                        x = Float.parseFloat(String.valueOf(event.values[0]));
+                        y = Float.parseFloat(String.valueOf(event.values[1]));
+                        z = Float.parseFloat(String.valueOf(event.values[2]));
+
+                        float speed = Math.abs(x + y+ z - last_x - last_y - last_z) / diffTime * 10000;
+
+                        if (speed > SHAKE_THRESHOLD) {
+                            Log.d("sensor", "shake detected w/ speed: " + speed);
+                        }
+
+                        last_x = x;
+                        last_y = y;
+                        last_z = z;
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                Log.d("ACCU_CHANGE", sensor.toString() + " - " + accuracy);
+            }
+        }, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void setupComponents() {
+        petSelectButton = (Button)findViewById(R.id.petSelectButton);
+        petSelectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, activity_sensors.class);
+                startActivity(intent);
+            }
+        });
     }
 }
